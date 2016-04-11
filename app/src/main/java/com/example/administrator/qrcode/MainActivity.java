d0010d516b0a13adc1472fb7a18838e4bcf6dce7
Tiipy <mixinan@163.com>
2016-04-12 00:46:24 +0800

    修改布局

--- app/src/main/java/com/example/administrator/qrcode/MainActivity.java
+++ app/src/main/java/com/example/administrator/qrcode/MainActivity.java
@@ -1,32 +1,37 @@
 package com.example.administrator.qrcode;
 
 import android.app.Activity;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.administrator.test.R;
 import com.xys.libzxing.zxing.activity.CaptureActivity;
 import com.xys.libzxing.zxing.encoding.EncodingUtils;
 
-public class MainActivity extends Activity {
-	private TextView tvResult;
+public class MainActivity extends Activity implements View.OnClickListener,View.OnLongClickListener {
 	private ImageView iv;
 	private EditText et;
+	private TextView tvResult, tvTitle;
+    private String result;
+	private ToastUtil toast = new ToastUtil();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		tvResult = (TextView) findViewById(R.id.textView1);
-		iv = (ImageView) findViewById(R.id.imageView1);
+
+		tvResult = (TextView) findViewById(R.id.result_text);
+		tvTitle = (TextView) findViewById(R.id.title);
+		tvTitle.setOnClickListener(this);
+		tvTitle.setOnLongClickListener(this);
+		iv = (ImageView) findViewById(R.id.imageView);
 		et = (EditText)findViewById(R.id.editText1);
 		
 	}
@@ -40,7 +45,7 @@ public class MainActivity extends Activity {
 		super.onActivityResult(requestCode, resultCode, data);
 		if(resultCode == RESULT_OK){
 			Bundle bundle = data.getExtras();
-			String result = bundle.getString("result");
+			result = bundle.getString("result");
 			tvResult.setText(result);
 		}
 	}
@@ -54,5 +59,97 @@ public class MainActivity extends Activity {
             Bitmap bitmap = EncodingUtils.createQRCode(string,500,500,null);
             iv.setImageBitmap(bitmap);
         }
+    }
+
+    public void doClick(View view) {
+    switch (view.getId()) {
+        case R.id.btCopy:
+            copy(result, getBaseContext());
+            toast.showToast(MainActivity.this, "已经复制到剪贴板");
+            break;
+
+        case R.id.btGo:
+            if (result.contains("http")||
+                    result.endsWith(".com")||
+                    result.endsWith(".cn")||
+                    result.endsWith(".cc")||
+                    result.endsWith(".html")) {
+                if (NetworkUtil.getNetWorkType(MainActivity.this) == NetworkUtil.NONETWORK) {
+                    toast.showToast(MainActivity.this, "没有联网");
+                } else {
+                    openTheNet(result);
+                }
+            } else {
+                toast.showToast(MainActivity.this, "这不是网址吧？");
+            }
+            break;
+
+        case R.id.btDial:
+            String telRegex = "[1][358]\\d{9}";
+            if (result.matches(telRegex)) {
+                dialTheNumber(result);
+            } else {
+                toast.showToast(MainActivity.this, "这不是电话吧？");
+            }
+            break;
+    }
+}
+
+
+    @Override
+    public void onClick(View v) {
+        toast.showToast(this, "作者：万码千钧");
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        chatWithMe();
+        return true;
+    }
+
+    private void chatWithMe() {
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        Uri net = Uri
+                .parse("http://wpd.b.qq.com/cgi/get_m_sign.php?uin=467662473");
+        intent.setData(net);
+        startActivity(intent);
+    }
+
+
+    public static void copy(String string, Context context) {
+        ClipboardManager cmb = (ClipboardManager) context
+                .getSystemService(Context.CLIPBOARD_SERVICE);
+        cmb.setText(string.trim());
+    }
+
+    private void dialTheNumber(String str) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + str));
+        startActivity(intent);
+    }
+
+    protected void openTheNet(String str) {
+        Intent openUrl_Intent = new Intent(this, WebActivity.class);
+        openUrl_Intent.putExtra("click", str);
+        startActivity(openUrl_Intent);
+    }
+
+
+    private long lastBackTime = 0;
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - lastBackTime < 1500) {
+            finish();
+        }
+        lastBackTime = System.currentTimeMillis();
+        toast.showToast(this, "再按一次退出");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/administrator/qrcode/WebActivity.java
@@ -0,0 +1,172 @@
+package com.example.administrator.qrcode;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by 万码千钧 on 2016/4/11.
+ */
+public class WebActivity extends Activity implements TextWatcher {
+
+    private WebView webView;
+    private ProgressDialog dialog;
+    private TextView tvHome;
+    private TextView tvNet;
+    private EditText etNet;
+    private ImageView etDelete;
+    private View etLayout;
+    private String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        Intent intent = getIntent();
+        url = intent.getStringExtra("click");
+
+        setWebView();
+        initViews();
+        addListeners();
+    }
+
+    protected void setWebView() {
+        webView = (WebView) findViewById(R.id.web_view);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setDisplayZoomControls(false);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    closeDialog();
+                } else {
+                    openDialog(newProgress);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(url);
+    }
+
+    private void initViews() {
+        tvHome = (TextView) findViewById(R.id.tv_home);
+        tvNet = (TextView) findViewById(R.id.tv_net);
+        tvNet.setText(url);
+        etNet = (EditText) findViewById(R.id.web_et);
+        etLayout = findViewById(R.id.web_et_layout);
+        etDelete = (ImageView) findViewById(R.id.web_et_delete);
+    }
+
+
+    private void addListeners() {
+        tvNet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                tvNet.setVisibility(View.GONE);
+                etLayout.setVisibility(View.VISIBLE);
+                etNet.setText(url);
+                etDelete.setVisibility(View.VISIBLE);
+            }
+        });
+
+        tvHome.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        etNet.addTextChangedListener(this);
+    }
+
+
+    private void closeDialog() {
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
+            dialog = null;
+        }
+    }
+
+    private void openDialog(int newProgress) {
+        if (dialog == null) {
+            dialog = new ProgressDialog(this);
+            dialog.setTitle("正在加载");
+            dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            dialog.setProgress(newProgress);
+            dialog.show();
+        } else {
+            dialog.setProgress(newProgress);
+        }
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (webView.canGoBack() && keyCode == KeyEvent.KEYCODE_BACK) {
+            webView.goBack();
+            return true;
+        } else {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void doClick(View v) {
+        switch (v.getId()) {
+            case R.id.web_et_delete:
+                etNet.setText("");
+                etDelete.setVisibility(View.GONE);
+                break;
+
+            case R.id.web_bt:
+                String net = GlobalConsts.BAIDU + etNet.getText().toString();
+                webView.loadUrl(net);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(
+                        this.getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+                break;
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable arg0) {
+    }
+    @Override
+    public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+    }
+    @Override
+    public void onTextChanged(CharSequence arg0, int arg1, int arg2, int count) {
+        if (count > 0) {
+            etDelete.setVisibility(View.VISIBLE);
+        }
+    }
+}
