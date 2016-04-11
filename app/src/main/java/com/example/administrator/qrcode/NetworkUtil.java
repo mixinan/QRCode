21b063ff0873a81b9976314adb1d3386276854d9
Tiipy <mixinan@163.com>
2016-04-12 00:23:25 +0800

    修改布局

---
+++ app/src/main/java/com/example/administrator/qrcode/NetworkUtil.java
@@ -0,0 +1,40 @@
+package com.example.administrator.qrcode;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtil {
+	public static final int NONETWORK = 0;
+	public static final int WIFI = 1;
+	public static final int NOWIFI = 2;
+
+	public static int getNetWorkType(Context context) {
+		if (!isNetWorkAvalible(context)) {
+			return NetworkUtil.NONETWORK;
+		}
+		
+		ConnectivityManager cm = (ConnectivityManager) 
+				context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		
+		if (cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting())
+			return NetworkUtil.WIFI;
+		else
+			return NetworkUtil.NOWIFI;
+	}
+
+	public static boolean isNetWorkAvalible(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) 
+				context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		
+		if (cm == null) {
+			return false;
+		}
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni == null || !ni.isAvailable()) {
+			return false;
+		}
+		return true;
+	}
+
+}
