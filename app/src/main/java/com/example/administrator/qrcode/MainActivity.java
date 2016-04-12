aa17f32dae7469343d79c2f0efc581ce3dc12624
Tiipy <mixinan@163.com>
2016-04-12 14:59:26 +0800

    修改bug

--- app/src/main/java/com/example/administrator/qrcode/MainActivity.java
+++ app/src/main/java/com/example/administrator/qrcode/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -17,40 +18,36 @@ import com.example.administrator.test.R;
 import com.xys.libzxing.zxing.activity.CaptureActivity;
 import com.xys.libzxing.zxing.encoding.EncodingUtils;
 
-public class MainActivity extends Activity implements View.OnClickListener,View.OnLongClickListener {
-	private ImageView iv;
-	private EditText et;
-	private TextView tvResult, tvTitle;
+public class MainActivity extends Activity {
+    private ImageView iv;
+    private EditText et;
+    private TextView tv;
     private String result;
-	private ToastUtil toast = new ToastUtil();
+    private ToastUtil toast = new ToastUtil();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		tvResult = (TextView) findViewById(R.id.result_text);
-		tvTitle = (TextView) findViewById(R.id.title);
-		tvTitle.setOnClickListener(this);
-		tvTitle.setOnLongClickListener(this);
-		iv = (ImageView) findViewById(R.id.imageView);
-		et = (EditText)findViewById(R.id.editText);
-		
-	}
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.imageView);
+        et = (EditText)findViewById(R.id.editText);
+    }
 
-	public void scan(View view){
-		startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class), 0);
-	}
+    public void scan(View view){
+        startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class), 0);
+    }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if(resultCode == RESULT_OK){
-			Bundle bundle = data.getExtras();
-			result = bundle.getString("result");
-			tvResult.setText(result);
-		}
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Bundle bundle = data.getExtras();
+            result = bundle.getString("result");
+            tv.setText(result);
+        }
+    }
 
 
     public void make(View view){
@@ -64,58 +61,35 @@ public class MainActivity extends Activity implements View.OnClickListener,View.
     }
 
     public void doClick(View view) {
-    switch (view.getId()) {
-        case R.id.btCopy:
-            copy(result, getBaseContext());
-            toast.showToast(MainActivity.this, "已经复制到剪贴板");
-            break;
+        if(!TextUtils.isEmpty(result)){
+            switch (view.getId()) {
 
-        case R.id.btGo:
-            if (result.contains("http")||
-                    result.endsWith(".com")||
-                    result.endsWith(".cn")||
-                    result.endsWith(".cc")||
-                    result.endsWith(".html")) {
-                if (NetworkUtil.getNetWorkType(MainActivity.this) == NetworkUtil.NONETWORK) {
-                    toast.showToast(MainActivity.this, "没有联网");
-                } else {
-                    openTheNet(result);
-                }
-            } else {
-                toast.showToast(MainActivity.this, "这不是网址吧？");
+                case R.id.btCopy:
+                    copy(result, getBaseContext());
+                    toast.showToast(MainActivity.this, "已经复制到剪贴板");
+                    break;
+
+                case R.id.btGo:
+                    if (result.contains("http") ||
+                            result.endsWith(".com") ||
+                            result.endsWith(".cn") ||
+                            result.endsWith(".cc") ||
+                            result.endsWith(".html")) {
+                        if (NetworkUtil.getNetWorkType(MainActivity.this) == NetworkUtil.NONETWORK) {
+                            toast.showToast(MainActivity.this, "没有联网");
+                        } else openTheNet(result);
+                    } else toast.showToast(MainActivity.this, "这不是网址吧？");
+                    break;
+
+                case R.id.btDial:
+                    String telRegex = "[1][358]\\d{9}";
+                    if (result.matches(telRegex)) {
+                        dialTheNumber(result);
+                    } else toast.showToast(MainActivity.this, "这不是电话吧？");
+                    break;
             }
-            break;
-
-        case R.id.btDial:
-            String telRegex = "[1][358]\\d{9}";
-            if (result.matches(telRegex)) {
-                dialTheNumber(result);
-            } else {
-                toast.showToast(MainActivity.this, "这不是电话吧？");
-            }
-            break;
-    }
-}
-
-
-    @Override
-    public void onClick(View v) {
-        toast.showToast(this, "作者：万码千钧");
-    }
-
-    @Override
-    public boolean onLongClick(View v) {
-        chatWithMe();
-        return true;
-    }
-
-    private void chatWithMe() {
-        Intent intent = new Intent();
-        intent.setAction("android.intent.action.VIEW");
-        Uri net = Uri
-                .parse("http://wpd.b.qq.com/cgi/get_m_sign.php?uin=467662473");
-        intent.setData(net);
-        startActivity(intent);
+        }else
+            toast.showToast(MainActivity.this,"内容为空");
     }
 
 
